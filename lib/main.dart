23df481382b5ef7ddecf525cf20aa14c05b7ac4a
Liight <Liight@users.noreply.github.com>
2020-02-20 14:12:33 +1300

    - add modal - refactor (removed widget)

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 // flutter
 import 'package:flutter/material.dart';
+import 'package:flutter_expenditure_tracker/widgets/new_transaction.dart';
 
 // custom
-import './widgets/user_transactions.dart';
+import './widgets/transactionList.dart';
+import './models/transaction.dart';
 
 // run app
 void main() => runApp(MyApp());
@@ -12,22 +14,67 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'Expenditure Tracker',
       home: MyHomePage(),
     );
   }
 }
 
 // main app state
-class MyHomePage extends StatelessWidget {
-  // user input variables
-  // String titleInput;
-  // String amountInput;
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
-  final titleController =
-      TextEditingController(); // flutter controller for text input
-  final amountController =
-      TextEditingController(); // flutter controller for text input
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 49.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'New Hat',
+      amount: 39.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't3',
+      title: 'New Sunglasses',
+      amount: 189.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't4',
+      title: 'Weekly Groceries',
+      amount: 129.55,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+      title: txTitle,
+      amount: txAmount,
+      date: DateTime.now(),
+      id: DateTime.now().toString(),
+    );
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return NewTransaction(_addNewTransaction);
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,12 +82,10 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(title: Text('Expenditure Tracker'), actions: <Widget>[
         IconButton(
           icon: Icon(Icons.add),
-          onPressed: () {},
+          onPressed: () => _startAddNewTransaction(context),
         ),
       ]),
       body: ListView(
-        // mainAxisAlignment: MainAxisAlignment.spaceAround,
-        // crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           Container(
             width: double.infinity,
@@ -52,13 +97,13 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          UserTransactions(),
+          TransactionList(_userTransactions),
         ],
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () => _startAddNewTransaction(context),
       ),
     );
   }
