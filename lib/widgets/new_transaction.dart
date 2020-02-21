6c1f30af571b7ed6ce1c913ef8bbdc7200c27d0d
Liight <Liight@users.noreply.github.com>
2020-02-21 16:14:19 +1300

    - add datePicker initial

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class NewTransaction extends StatefulWidget {
   final Function addTx;
@@ -10,17 +11,17 @@ class NewTransaction extends StatefulWidget {
 }
 
 class _NewTransactionState extends State<NewTransaction> {
-  final titleController = TextEditingController();
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime _selectedDate;
 
-  final amountController = TextEditingController();
+  void _submitData() {
+    final enteredTitle = _titleController.text;
+    final enteredAmount = double.parse(_amountController.text);
 
-  void submitData() {
-    final enteredTitle = titleController.text;
-    final enteredAmount = double.parse(amountController.text);
-
-  if(enteredTitle.isEmpty || enteredAmount <= 0){
-    return;
-  }
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
 
     // widget is provided and gives access to statefulWidget properties
     widget.addTx(
@@ -29,6 +30,22 @@ class _NewTransactionState extends State<NewTransaction> {
     );
 
     Navigator.of(context).pop(); // close modal
+  }
+
+  void _presentDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2020),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
   }
 
   @override
@@ -43,23 +60,48 @@ class _NewTransactionState extends State<NewTransaction> {
             TextField(
               decoration: InputDecoration(labelText: 'Title'),
               controller:
-                  titleController, // using the flutter controller for text input
+                  _titleController, // using the flutter controller for text input
               // onChanged: (value){ titleInput = value; },
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
               controller:
-                  amountController, // using the flutter controller for text input
+                  _amountController, // using the flutter controller for text input
               keyboardType: TextInputType.numberWithOptions(
                   decimal: true), // required for IOS check
               onSubmitted: (_) =>
-                  submitData, // not using (_), just submitData, required acceptance for syntax
+                  _submitData, // not using (_), just submitData, required acceptance for syntax
               // onChanged: (value){ amountInput = value; },
             ),
-            FlatButton(
+            Container(
+              height: 70,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      _selectedDate == null
+                          ? 'No Date Chosen'
+                          : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}',
+                    ),
+                  ),
+                  FlatButton(
+                    textColor: Theme.of(context).primaryColor,
+                    child: Text(
+                      'Choose Date',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    onPressed: _presentDatePicker,
+                  )
+                ],
+              ),
+            ),
+            RaisedButton(
               child: Text('Add Transaction'),
-              textColor: Colors.purple,
-              onPressed: submitData,
+              color: Theme.of(context).primaryColor,
+              textColor: Theme.of(context).textTheme.button.color,
+              onPressed: _submitData,
             ),
           ],
         ),
