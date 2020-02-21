6155b6128f1762c2cc268d92acc98041c2005f72
Liight <Liight@users.noreply.github.com>
2020-02-21 16:17:35 +1300

    - datePicker logic

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -16,10 +16,13 @@ class _NewTransactionState extends State<NewTransaction> {
   DateTime _selectedDate;
 
   void _submitData() {
+    if(_amountController.text.isEmpty){
+      return;
+    }
     final enteredTitle = _titleController.text;
     final enteredAmount = double.parse(_amountController.text);
 
-    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+    if (enteredTitle.isEmpty || enteredAmount <= 0 || _selectedDate == null) {
       return;
     }
 
@@ -27,6 +30,7 @@ class _NewTransactionState extends State<NewTransaction> {
     widget.addTx(
       enteredTitle,
       enteredAmount,
+      _selectedDate
     );
 
     Navigator.of(context).pop(); // close modal
